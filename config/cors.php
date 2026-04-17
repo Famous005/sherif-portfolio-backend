3aa2f7d02d5aac637d9e6a69e31345d2c2770ea0
Famous005 <badamasisherif8@gmail.com>
2026-04-17 14:40:29 -0700

    Fix database seeding configuration

--- config/cors.php
+++ config/cors.php
@@ -6,6 +6,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://sherif-portfolio-frontend.vercel.app/'.
         env('FRONTEND_URL', 'http://localhost:5173'),
     ],
     'allowed_origins_patterns' => [],
